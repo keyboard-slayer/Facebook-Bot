88cebf7c3fe113e55681a60fc14d71b630e1a87a
0v3rl0w <dalcjor@libmail.eu>
2019-02-02 21:11:13 +0100

    Reloaded the bot is perfect

--- README.md
+++ README.md
@@ -3,11 +3,3 @@
 This bot use Cleverbot (not the Api, too expensive)
 
 Install the libary from [Here](https://gitlab.com/0v3rl0w/Unofficial-Cleverbot-Api)
-
-
-## How to install ?
-
-```
-sudo apt install python2.7 python2-pip
-python2.7 bot.py
-```
--- bot.py
+++ bot.py
@@ -1,80 +1,71 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import selenium
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from getpass import getpass
-import time
+from getpass import getpass 
+from cleverbot import Cleverbot
+
 import os
 
-
-from cleverbot import * # For this git: https://github.com/0v3rl0w/Unofficial-Cleverbot-Api
-
-class Facebook(object):
+class Messenger:
     def __init__(self):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Firefox() # Because Firefox is better 
         self.driver.get("https://www.messenger.com")
-        self.login()
-        os.system("clear")
-        str(input("Press Enter when you're ready to spam..."))
-        self.name = self.getName()
 
-    def login(self):
-        self.driver.find_element_by_id("email").send_keys(str(input("Email Address: ")))
-        self.driver.find_element_by_id("pass").send_keys(getpass(prompt="Password: "))
+    def login(self, username: str, password: str):
+        self.driver.find_element_by_id("email").send_keys(username)
+        self.driver.find_element_by_id("pass").send_keys(password)
         self.driver.find_element_by_name("login").click()
 
-    def inMsg(self, msg):
-        msgRec = self.driver.find_elements_by_xpath("//*[contains(@class, '_aok')]")
-
-        for received in msgRec:
-             if msg == received.text:
-                 return True
-        return False
-
-    def getMsg(self):
-         return self.driver.find_elements_by_xpath("//*[contains(@class, '_aok')]")[-1].text
-
-    def getName(self):
+    def getReceiverName(self) -> str:
         name = self.driver.find_element_by_class_name("_3oh-").text
         return name.replace("à", "a").replace("ï", "i").replace("ô", "o").replace("ê", "e").replace("é", "e").replace("è", "e").lower()
 
-    def sendMsg(self, msg):
+    def getMsg(self) -> str:
+        return self.driver.find_elements_by_xpath("//*[contains(@class, '_aok')]")[-1].text
+
+    def send(self, msg):
         try:
             self.driver.find_element_by_class_name("notranslate").send_keys(msg)
             self.driver.find_element_by_class_name("_38lh").click()
 
         except selenium.common.exceptions.NoSuchElementException:
-            return False
-if __name__ == "__main__":
-    facebook = Facebook()
-    bot = Cleverbot()
-    print(facebook.name.split(' ')[0])
-    lastestMsg = ""
-    bot.send("Je m'appelle {}".format(facebook.name.split(' ')[0]))
-    if not facebook.inMsg("Bonjour {}".format(facebook.name.split(' ')[0])):
-        facebook.sendMsg("Bonjour {}".format(facebook.name.split(' ')[0]))
-        lastestMsg = "Bonjour {}".format(facebook.name.split(' ')[0])
-    else:
-        code = str(facebook.getMsg())
-        print(code)
-        bot.send(code)
-        lastestMsg = bot.get().lower()
-        print(lastestMsg)
-        facebook.sendMsg(lastestMsg)
+            return 1
 
-    lastestTmp = ""
+    def quit(self):
+        self.driver.quit()
+
+def waitAnswer(msg=""):
+    while ai.get() == msg:
+        continue
+    return True
+
+if __name__ == "__main__":
+    lastestMsg = ""
+    bot = Messenger()
+    print("Loading the Cleverbot API...")
+    ai = Cleverbot()
+    
+    username = input("Login: ")
+    password = getpass(prompt="Password: ")
+
+    ai.send("Hi, my name is {bot.getReceiverName()} ")
+    
+    waitAnswer()
+    bot.send(ai.get())
+    lastestMsg = ai.get()
+
+
     while True:
-        received = facebook.getMsg()
-        time.sleep(1)
-        print(received)
-        if received != lastestMsg:
-            lastestTmp = lastestMsg
-            try:
-                bot.send(str(received))
-            except:
-                pass
-            lastestMsg = bot.get().lower()
+        if bot.getMsg() != lastestMsg:
             print(lastestMsg)
-            if "appelle" not in lastestMsg and lastestMsg != lastestTmp:
-                facebook.sendMsg(lastestMsg)
+            ai.send(bot.getMsg())
+            waitAnswer(lastestMsg)
+            bot.send(ai.get())
+            lastestMsg = ai.get()
+
+
+    bot.quit()
+    exit()
+    
